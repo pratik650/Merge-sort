6aad0bff779361d6dcc4d29a7220f399aa49eefb
Pratik Kumbhkar <pratikprajapati650@gmail.com>
2022-06-07 20:29:00 +0530

    Delete portfolio.cpp

--- portfolio.cpp
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv...>
-   <link rel="stylesheet" href="css/styles.css?v=1.0">
